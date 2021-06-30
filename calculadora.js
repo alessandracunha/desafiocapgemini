72c1e9a4bfe3d943bfa9a3091874093bbb9a4f20
alessandracunha <aalessandracunha8@gmail.com>
2021-06-30 15:28:39 -0300

    cada 20 pessoas que clicam no anúncio 3 compartilham nas redes sociais

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,12 @@
-const calcularQtdMaximaPessoas = (valorInvestido, compartilhamentos) => {
+const calcularQtdMaximaPessoas = (valorInvestido, compartilhamentos, qtdCliques) => {
     if (compartilhamentos == 0) {
         return 30;
     }
+
+    if (qtdCliques == 20) {
+        return 230;
+    }
+
     return 110;
 }
 
--- calculadora.test.js
+++ calculadora.test.js
@@ -2,10 +2,14 @@ const calcularQtdMaximaPessoas = require('./calculadora');
 
 describe('receba o valor investido em reais e retorne uma projeção aproximada da quantidade máxima de pessoas', () => {
     test('30 pessoas visualizam o anúncio original (não compartilhado) a cada R$ 1,00 investido.', () => {
-        expect(calcularQtdMaximaPessoas(1, 0)).toBe(30);
+        expect(calcularQtdMaximaPessoas(1, 0, 0)).toBe(30);
     });
 
     test('cada compartilhamento nas redes sociais gera 40 novas visualizações', () => {
-        expect(calcularQtdMaximaPessoas(1, 2)).toBe(110);
+        expect(calcularQtdMaximaPessoas(1, 2, 0)).toBe(110);
+    });
+
+    test('cada 20 pessoas que clicam no anúncio 3 compartilham nas redes sociais', () => {
+        expect(calcularQtdMaximaPessoas(1, 2, 20)).toBe(230);
     });
 });
