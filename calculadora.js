aeda0d800a0794669e69db1df3564ed25da0116a
alessandracunha <aalessandracunha8@gmail.com>
2021-06-30 16:22:36 -0300

    limpar código

--- calculadora.js
+++ calculadora.js
@@ -9,15 +9,19 @@ const calcularQtdMaximaPessoas = (valorInvestido, compartilhamentos, qtdCliques)
         qtdVisualizacoes = 110;
     }
 
-    let visualizacoesBase = 100;
-    let cliquesBase = 12;
-    const cliquesResult = Math.round((qtdVisualizacoes * cliquesBase) / visualizacoesBase);
+    return calcularQtdCompartilhamentos(qtdVisualizacoes) * 40 + qtdVisualizacoes;
+}
 
-    cliquesBase = 20;
-    let compartilhamentosBase = 3;
-    const compartilhamentosResult = Math.round((cliquesResult * compartilhamentosBase) / cliquesBase);
-    
-    return compartilhamentosResult * 40 + qtdVisualizacoes;
+const calcularQtdCompartilhamentos = (qtdVisualizacoes) => {
+    const cliquesBase = 20;
+    const compartilhamentosBase = 3;
+    return Math.round((calcularQtdCliques(qtdVisualizacoes) * compartilhamentosBase) / cliquesBase);
+}
+
+const calcularQtdCliques = (qtdVisualizacoes) => {
+    const visualizacoesBase = 100;
+    const cliquesBase = 12;
+    return Math.round((qtdVisualizacoes * cliquesBase) / visualizacoesBase);
 }
 
 module.exports = calcularQtdMaximaPessoas;
