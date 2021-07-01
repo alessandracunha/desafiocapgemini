e687880128090de206ab12283c5b1d5670371c53
alessandracunha <aalessandracunha8@gmail.com>
2021-07-01 20:38:01 -0300

    adequação dos testes unitários de acordo com a nova regra

--- calculadora.test.js
+++ calculadora.test.js
@@ -2,15 +2,15 @@ const calcularQtdMaximaPessoas = require('./calculadora');
 
 describe('receba o valor investido em reais e retorne uma projeção aproximada da quantidade máxima de pessoas', () => {
     test('30 pessoas visualizam o anúncio original (não compartilhado) a cada R$ 1,00 investido.', () => {
-        expect(calcularQtdMaximaPessoas(1, 0, 0)).toBe(30);
+        expect(calcularQtdMaximaPessoas(1, 0, 0)).toBe(70);
     });
 
     test('cada compartilhamento nas redes sociais gera 40 novas visualizações', () => {
-        expect(calcularQtdMaximaPessoas(1, 2, 0)).toBe(110);
+        expect(calcularQtdMaximaPessoas(1, 2, 0)).toBe(190);
     });
 
     test('cada 20 pessoas que clicam no anúncio 3 compartilham nas redes sociais', () => {
-        expect(calcularQtdMaximaPessoas(1, 2, 20)).toBe(230);
+        expect(calcularQtdMaximaPessoas(1, 2, 20)).toBe(390);
     });
     
     test('cada 100 pessoas que visualizam o anúncio 12 clicam nele', () => {
