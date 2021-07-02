3afdb714410d68fd81e86d8748532ea750933545
alessandracunha <aalessandracunha8@gmail.com>
2021-07-01 22:02:33 -0300

    documentação das funções

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,7 @@
+/**
+ * @param {*} valorInvestido 
+ * @description Recebe um valor investido em reais e retorna uma projeção aproximada da quantidade máxima de pessoas que visualizarão o mesmo anúncio
+ */
 const calcularQtdMaximaPessoas = (valorInvestido) => {
     const qtdVisualizacoesOriginal = 30;
     const qtdVisualizacoes = valorInvestido * qtdVisualizacoesOriginal;
@@ -8,6 +12,13 @@ const calcularQtdMaximaPessoas = (valorInvestido) => {
     return qtdCompartilhamentos * qtdNovasVisualizacoesPorCompartilhamento + qtdVisualizacoes;
 }
 
+/**
+ * 
+ * @param {*} qtdCliques 
+ * @description A cada 20 pessoas que clicam no anúncio 3 compartilham e no máximo são compartilhados 4 em sequência
+ * Recebe a quantidade de cliques como referência e aplica uma regra de 3 nesse valor base de cliques multiplicando pelo máximo em sequência 
+ * Como não existe meio clique se arredonda o valor do resultado da regra de 3
+ */
 const calcularQtdCompartilhamentos = (qtdCliques) => {
     const cliquesBase = 20;
     const compartilhamentosBase = 3;
@@ -15,6 +26,13 @@ const calcularQtdCompartilhamentos = (qtdCliques) => {
     return Math.round((qtdCliques * compartilhamentosBase) / cliquesBase) * sequenciaMaximaCompartilhamentos;
 }
 
+/**
+ * 
+ * @param {*} qtdVisualizacoes 
+ * @description A cada 100 pessoas que visualizam o anúncio 12 clicam nele
+ * Recebe a quantidade de vizualizações como referência e aplica regra de 3 no valor base de vizualizações 
+ * Como não existe meio clique se arredonda o valor do resultado da regra de 3
+ */
 const calcularQtdCliques = (qtdVisualizacoes) => {
     const visualizacoesBase = 100;
     const cliquesBase = 12;
